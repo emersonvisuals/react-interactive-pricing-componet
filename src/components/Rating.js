aeffdea666ef2caa3276a9d220af77d23b4bdc85
Thomas Emerson <emersonvisuals@gmail.com>
2022-11-30 12:15:43 +1100

    updated website

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,13 +1,71 @@
 import React from "react";
 
 const Rating = () => {
-    const numbers = [1, 2, 3, 4, 5];
-    const listItems = numbers.map((number) => <li>{number}</li>);
+    // items
+    let listOne = document.querySelector("li.one");
+    let listTwo = document.querySelector("li.two");
+    let listThree = document.querySelector("li.three");
+    let listFour = document.querySelector("li.four");
+    let listFive = document.querySelector("li.five");
+
+    const itemOne = () => {
+        listOne.classList.toggle("active");
+        listTwo.classList.remove("active");
+        listThree.classList.remove("active");
+        listFour.classList.remove("active");
+        listFive.classList.remove("active");
+    };
+
+    const itemTwo = () => {
+        listOne.classList.remove("active");
+        listTwo.classList.toggle("active");
+        listThree.classList.remove("active");
+        listFour.classList.remove("active");
+        listFive.classList.remove("active");
+    };
+
+    const itemThree = () => {
+        listOne.classList.remove("active");
+        listTwo.classList.remove("active");
+        listThree.classList.toggle("active");
+        listFour.classList.remove("active");
+        listFive.classList.remove("active");
+    };
+
+    const itemFour = () => {
+        listOne.classList.remove("active");
+        listTwo.classList.remove("active");
+        listThree.classList.remove("active");
+        listFour.classList.toggle("active");
+        listFive.classList.remove("active");
+    };
+
+    const itemFive = () => {
+        listOne.classList.remove("active");
+        listTwo.classList.remove("active");
+        listThree.classList.remove("active");
+        listFour.classList.remove("active");
+        listFive.classList.toggle("active");
+    };
 
     return (
         <div className="ratingContainer">
-            <div key="list" className="items">
-                {listItems}
+            <div className="items">
+                <li className="one" onClick={itemOne}>
+                    1
+                </li>
+                <li className="two" onClick={itemTwo}>
+                    2
+                </li>
+                <li className="three" onClick={itemThree}>
+                    3
+                </li>
+                <li className="four" onClick={itemFour}>
+                    4
+                </li>
+                <li className="five" onClick={itemFive}>
+                    5
+                </li>
             </div>
             <button>submit</button>
         </div>
---
+++ src/components/Thankyou.js
@@ -0,0 +1,27 @@
+import ThankyouImg from "../images/illustration-thank-you.svg";
+
+const Thankyou = () => {
+    return (
+        <div className="thankYouContainer">
+            <div className="imageContainerNew">
+                <img
+                    className="imageThankyou"
+                    src={ThankyouImg}
+                    alt="thankyou image"
+                />
+            </div>
+            <div className="selectionContainer">
+                <p>You selected 4 out of 5</p>
+            </div>
+            <div className="thankYouContainer">
+                <h1>Thank you!</h1>
+                <p>
+                    We appreciate you taking the time to give a rating. If you
+                    ever need more support, don't hesitate to get in touch!
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Thankyou;
